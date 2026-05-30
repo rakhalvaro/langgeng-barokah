a144fc085439207c0afb3b3b75c3a5b2884d2923
Moh. Rakha Farras Alvaro <mohammadrakha2@gmail.com>
2026-05-30 22:29:55 +0700

    fix: update package name to langgeng_barokah

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.berkah_telor"
+    namespace = "com.example.langgeng_barokah"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.berkah_telor"
+        applicationId = "com.example.langgeng_barokah"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
